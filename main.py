e1e028aa958a6e565fea58741c011ee59f2448a3
John <johnmichaelab6@gmail.com>
2024-10-15 17:45:29 -0600

    Codigo base del juego

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("proyecto software")
+import pygame
+import sys
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+running = True
+
+
+while running:
+    screen.fill("black")
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
+pygame.quit()
